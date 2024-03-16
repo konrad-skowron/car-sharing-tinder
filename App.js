197cf1b7d0e150e0446546a9ba8c4cd85d387a82
Konrad Skowron <73618275+Konrad-Skowron@users.noreply.github.com>
2024-03-16 20:04:36 +0100

    react native and firebase auth setup

--- App.js
+++ App.js
@@ -1,10 +1,70 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { GoogleSignin, GoogleSigninButton } from '@react-native-google-signin/google-signin';
+import auth from '@react-native-firebase/auth';
+import React, { useState, useEffect } from 'react';
+import { Button } from 'react-native';
+import 'expo-dev-client';
 
 export default function App() {
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  GoogleSignin.configure({
+    webClientId: '404310386523-hidmeiimceqv2sc29c3gmsgfda99m20i.apps.googleusercontent.com',
+  });
+
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (initializing) return null;
+
+  async function onGoogleButtonPress() {
+    // Check if your device supports Google Play
+    await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
+    // Get the users ID token
+    const { idToken } = await GoogleSignin.signIn();
+  
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+  
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(googleCredential);
+  }
+
+  async function signOut() {
+    try {
+      await GoogleSignin.revokeAccess();
+      await auth().signOut();
+      setUser(null);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  if (!user) {
+    return (
+      <View style={styles.container}>
+        <GoogleSigninButton
+          onPress={onGoogleButtonPress}
+        />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Siema</Text>
+      <Button title="Sign out" onPress={signOut} />
       <StatusBar style="auto" />
     </View>
   );
