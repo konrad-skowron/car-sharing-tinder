ad51e7b3e2e4323a561b7a17e41e8bdd4db42853
Bartosz Sadowski <sadziu01@gmail.com>
2024-04-26 16:44:37 +0200

    Scrolling the Home page

--- app/Home.js
+++ app/Home.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useCallback } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, Button, Image } from "react-native";
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Button, Image, ScrollView } from "react-native";
 import app from "../firebaseConfig";
 import { getFirestore, collection, query, where, getDocs } from "firebase/firestore";
 import { Link, Stack, useRouter, useFocusEffect } from "expo-router";
@@ -87,72 +87,74 @@ const Home = () => {
 
   return (
     <View style={styles.container}>
-      <Stack.Screen
-        options={{
-          headerShadowVisible: false,
-          headerTitle: "",
-          headerStyle: { backgroundColor: "#eee" },
-          headerLeft: () => (
-            <AntDesign
-              name="book"
-              style={{ marginLeft: 16 }}
-              size={28}
-              color={COLORS.darkGray}
-              onPress={() => {
-                router.navigate("/user/matched");
-              }}
-            />
-          ),
-          headerRight: () => (
-            <TouchableOpacity
-              onPress={() => {
-                router.navigate("./user/About");
-              }}
-            >
-              <Image
-                style={{
-                  height: 48,
-                  width: 48,
-                  borderRadius: 48,
-                  marginRight: 16,
-                }}
-                source={{
-                  uri: "https://picsum.photos/200",
+      <ScrollView>
+        <Stack.Screen
+          options={{
+            headerShadowVisible: false,
+            headerTitle: "",
+            headerStyle: { backgroundColor: "#eee" },
+            headerLeft: () => (
+              <AntDesign
+                name="book"
+                style={{ marginLeft: 16 }}
+                size={28}
+                color={COLORS.darkGray}
+                onPress={() => {
+                  router.navigate("/user/matched");
                 }}
               />
-            </TouchableOpacity>
-          ),
-        }}
-      />
-      <View>
-        <Text
-          style={{
-            fontSize: 16,
-            fontFamily: FONTS.primaryRegular,
-            marginBottom: 4,
+            ),
+            headerRight: () => (
+              <TouchableOpacity
+                onPress={() => {
+                  router.navigate("./user/About");
+                }}
+              >
+                <Image
+                  style={{
+                    height: 48,
+                    width: 48,
+                    borderRadius: 48,
+                    marginRight: 16,
+                  }}
+                  source={{
+                    uri: "https://picsum.photos/200",
+                  }}
+                />
+              </TouchableOpacity>
+            ),
           }}
-        >
-          Available:
-        </Text>
-        <View style={styles.tileContainer}>
-          {!rides.length ||
-            rides.map((ride, index) => (
-              <Link key={index} href={`ride/${ride.index}`} asChild>
-                <TouchableOpacity>
-                  <RideTile ride={ride} />
-                </TouchableOpacity>
-              </Link>
-            ))}
+        />
+        <View>
+          <Text
+            style={{
+              fontSize: 16,
+              fontFamily: FONTS.primaryRegular,
+              marginBottom: 4,
+            }}
+          >
+            Available:
+          </Text>
+          <View style={styles.tileContainer}>
+            {!rides.length ||
+              rides.map((ride, index) => (
+                <Link key={index} href={`ride/${ride.index}`} asChild>
+                  <TouchableOpacity>
+                    <RideTile ride={ride} />
+                  </TouchableOpacity>
+                </Link>
+              ))}
+          </View>
         </View>
-      </View>
-      <View style={{ position: "fixed", alignSelf: "flex-end" }}>
-        <Link href="ride/new/StartLocationPick" asChild>
-          <TouchableOpacity style={styles.plusButton}>
-            <AntDesign name="plus" size={24} color="#eee" style={{ padding: 14 }} />
-          </TouchableOpacity>
-        </Link>
-      </View>
-      <StatusBar style="auto" />
+        </ScrollView>
+        <View style={{ position: "fixed", alignSelf: "flex-end", paddingTop: 16,}}>
+          <Link href="ride/new/StartLocationPick" asChild>
+            <TouchableOpacity style={styles.plusButton}>
+              <AntDesign name="plus" size={24} color="#eee" style={{ padding: 14 }} />
+            </TouchableOpacity>
+          </Link>
+        </View>
+        <StatusBar style="auto" />
     </View>
   );
 };
@@ -165,10 +167,8 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    display: "flex",
-    flexDirection: "column",
-    rowGap: 16,
-    padding: 32,
+    paddingHorizontal: 32,
+    paddingBottom: 16,
     backgroundColor: "#eee",
   },
   tileContainer: {
