3ed2834587c88540e41066024185ff2e26fab8f4
Bartosz Sadowski <sadziu01@gmail.com>
2024-05-20 19:45:36 +0200

    fix removing and add Failed page

---
+++ app/ride/new/Failed.js
@@ -0,0 +1,69 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import MainButton from "../../../components/MainButton";
+import { COLORS, FONTS } from "../../../constants";
+import { AntDesign } from "@expo/vector-icons";
+import { Stack } from "expo-router";
+import { useNewRideContext } from "../../../context/NewRideProvider";
+
+const Success = () => {
+  const { addOffer } = useNewRideContext();
+
+  return (
+    <View style={styles.container}>
+      <Stack.Screen options={{ headerShown: false }} />
+      <Text style={styles.title}>Something went wrong :c</Text>
+      <View style={styles.outer}>
+        <View style={styles.inner}>
+          <Ionicons name="checkmark-outline" size={128} color={COLORS.white} />
+        </View>
+      </View>
+      <MainButton href="../../Home" content="Home" onPress={() => addOffer()} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    display: "flex",
+    flexDirection: "column",
+    rowGap: 50,
+    padding: 32,
+    backgroundColor: "#eee",
+  },
+  outer: {
+    marginTop: 48,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: "auto",
+    marginRight: "auto",
+    width: 256,
+    height: 256,
+    borderRadius: 256,
+    backgroundColor: 'red',
+  },
+  inner: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: "auto",
+    marginRight: "auto",
+    width: 208,
+    height: 208,
+    borderRadius: 208,
+    borderWidth: 5,
+    borderColor: COLORS.white,
+    backgroundColor: 'red',
+  },
+  title: {
+    marginTop: 64,
+    fontSize: 42,
+    fontFamily: FONTS.secondaryBold,
+    textAlign: "center",
+  },
+});
+
+export default Success;
--- context/DataProvider.js
+++ context/DataProvider.js
@@ -130,6 +130,17 @@ const DataProvider = ({ children }) => {
         rides: arrayRemove(rideId),
       });
 
+      for (const userId of rideToDelete.passengers) {
+        const docSnapUser = await getDoc(doc(db, "users", userId));
+        const userData = docSnapUser.data();
+  
+        if (userData?.matched?.includes(rideId)) {
+          await updateDoc(doc(db, "users", userId), {
+            matched: arrayRemove(rideId)
+          });
+        }
+      }
+
       for (const day of rideToDelete.days) {
         await updateDoc(doc(db, day, timeId.toString()), {
           rides: arrayRemove(rideId),
